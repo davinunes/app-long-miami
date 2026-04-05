3be42b66a69589c4f5139c90078a7e47b813fcd8
Davi Nunes <davinunes.franca@eb.mil.br>
2026-04-05 01:21:13 -0300

    delete

--- migrates/001_initial_schema.sql
+++
@@ -1,4 +0,0 @@
--- Senha: umaSenhaMuitoForte123! (já com hash bcrypt)
-INSERT INTO usuarios (nome, email, senha, role) VALUES 
-    ('Administrador', 'admin@seusistema.com', '$2y$10$gIlV6Aw0DXP0cN67L5kHAOz6NxzekmybdC8VkjPTBGVAuReonpI0C', 'admin')
-ON DUPLICATE KEY UPDATE nome = VALUES(nome);
